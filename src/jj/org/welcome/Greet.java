0c210c4fbbd694028bed5436a090dc00d9437d18
jensonjoseph <jenson.joseph@outlook.com>
2015-08-18 15:25:06 -0400

    added Utils class and used it in Greet

--- src/jj/org/welcome/Greet.java
+++ src/jj/org/welcome/Greet.java
@@ -4,6 +4,8 @@ public class Greet {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
+		System.out.println("Enhancement 1");
+		Utils.greet();
 	}
 
 }
---
+++ src/jj/org/welcome/Utils.java
@@ -0,0 +1,7 @@
+package jj.org.welcome;
+
+public class Utils {
+	public static void greet() {
+		System.out.println("Hello from Utils ");
+	}
+}
